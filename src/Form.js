636dedab90e7420a2b4de7b959df4eacbb4a0775
heimzza <hamzassm18@gmail.com>
2022-07-17 15:09:29 +0300

    props ctor

--- src/Form.js
+++ src/Form.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 
 class Form extends Component {
 
-    addButton()
-    {
+    constructor(props){
+        super(props);
+        // this.addButton = this.addButton.bind(this);
+    }
+    addButton = () => {
         this.props.addNews();
         console.log('Hi');
     }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.addButton.bind(this)}>Ekle</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <button onClick={this.addButton}>Ekle</button>
+            </div>
+        )
+    }
 }
 
 export default Form;
